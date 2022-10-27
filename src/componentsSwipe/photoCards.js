e16e0a44f16a7a8efc60de8ba5cf1f2b8fd63e08
Réminho <remi.corroy@gmail.com>
2022-10-27 18:47:11 +0200

    push first

---
+++ src/componentsSwipe/photoCards.js
@@ -0,0 +1,32 @@
+const photoCards = [
+    { name: 'Nico Marks',
+    age: 24,
+    photo: require('../../assets/photos/IMG_3964.jpeg'),
+    key: 'mFcc5b_t74Q',
+    },
+    {
+    name: 'Sirio',
+    age: 28,
+    photo: require('../../assets/photos/IMG_3964.jpeg'),
+    key: "Ty4f_NOFO60'",
+    },
+    {
+    name: 'Teymi Townsend',
+    age: 30,
+    photo: require('../../assets/photos/IMG_4089.jpeg'),
+    key: "AvLHH8qYbAI'",
+    },
+    {
+    name: 'Caique Silva',
+    age: 20,
+    photo: require('../../assets/photos/IMG_4124.jpeg'),
+    key: "3ujVzg9i2EI'",
+    },
+    {
+    name: 'David Yanutenama',
+    age: 21,
+    photo: require('../../assets/photos/IMG_4146.jpeg'),
+    key: "5AoO7dBurMw'",
+    },
+    ]
+    export default photoCards
